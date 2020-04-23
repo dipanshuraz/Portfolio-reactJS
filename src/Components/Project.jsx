0809bba010424f9d7abbded9c9548e4a1cf1eb02
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-04-23 11:16:33 +0530

    new added

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -9,6 +9,21 @@ export class Project extends Component {
 
       data: [
         {
+          name: "Employee Management System",
+          desc:
+            "Employee Management System is a mini project created using ReactJs and have features like create & delete with custom modal and tables.",
+          img: "/project-demo/emp2.png",
+          demoLink: "https://elegant-elion-24e194.netlify.app/",
+          codeLink: "https://github.com/dipanshuraz/saarthi.ai_assessment_front-end",
+          techstack: [
+            "/icons/html-5.svg",
+            "/icons/css-3.svg",
+            "/icons/javascript.svg",
+            "/icons/react.svg",
+            "/icons/redux.svg",
+          ]
+        },
+        {
           name: "Truck Tracking System with Realtime API",
           desc:
             "Truck Tracking System with Realtime API from mystral and have featurs like Filtering and Searching. Used Google Maps in React & Used Ant Design for UI Framework.",
