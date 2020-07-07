dd2667058e9b4586470c34d67c2921dfcbbd086b
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-07-07 20:26:31 +0530

    experience page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
-import Home from "./Components/Home";
-import Project from "./Components/Project";
-import Achievements from "./Components/Achievements";
-import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
-import Blogs from "./Components/Blogs";
+import React from 'react';
+import './App.css';
+import Home from './Components/Home';
+import Project from './Components/Project';
+import Achievements from './Components/Achievements';
+import Experiences from './Components/Experience';
+import { Link, Route, BrowserRouter, Switch } from 'react-router-dom';
+import Blogs from './Components/Blogs';
 
 function App() {
   return (
@@ -24,10 +25,15 @@ function App() {
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto d-flex justify-content-around w-100">
+            <ul class="navbar-nav mr-auto d-flex justify-content-around w-100 mt-3">
               <li className="font-weight-bold lead">
                 <Link to="/" className="text-dark">
                   Home
+                </Link>
+              </li>
+              <li className="font-weight-bold lead">
+                <Link to="/experiences" className="text-dark">
+                  Experiences
                 </Link>
               </li>
               <li className="font-weight-bold lead">
@@ -51,6 +57,7 @@ function App() {
         <hr />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/experiences" component={Experiences} />
           <Route path="/projects" component={Project} />
           <Route path="/achievements" component={Achievements} />
           <Route path="/blogs" component={Blogs} />
---
+++ src/Components/Experience.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+export class Achievements extends Component {
+  componentDidMount() {
+    document.title = 'Deepanshu | Experiences';
+  }
+  render() {
+    return (
+      <div className="container text-dark">
+        <hr />
+        <h2 className="font-weight-bold"># Experiences</h2>
+        <hr />
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <div className="row my-5">
+              <div className="col-md-8">
+                <h4 className="font-weight-bold">
+                  <h6 className="font-weight-bold">May 2020 - July 2020</h6>
+                  <code>&gt;</code> Frontend Developer (Intern)
+                </h4>
+
+                <p className="lead font-weight-bold">
+                  Kuku FM <br />
+                  Mumbai, Maharashtra
+                </p>
+
+                <p className="lead ">
+                  Revamped the User Interface & Experience on the live website
+                  www.kukufm.com & Creator platform. Reported directly to CTO &
+                  participated in strategy and decision making for the
+                  development team.
+                </p>
+              </div>
+            </div>
+            <div className="row my-5">
+              <div className="col-md-8">
+                <h4 className="font-weight-bold">
+                  <h6 className="font-weight-bold">Feb 2020 - May 2020</h6>
+                  <code>&gt;</code> Web Developer (Intern)
+                </h4>
+
+                <p className="lead font-weight-bold">
+                  GirlScript Foundation <br />
+                  Remote
+                </p>
+
+                <p className="lead ">
+                  My roles and responsibilities include leading the project
+                  "Educational Platform" initiated by the company.
+                </p>
+              </div>
+            </div>
+
+            <div className="row my-5">
+              <div className="col-md-8">
+                <h4 className="font-weight-bold">
+                  <h6 className="font-weight-bold">Oct 2019 - Apr 2020</h6>
+                  <code>&gt;</code> Full Stack Developer (Trainee)
+                </h4>
+
+                <p className="lead font-weight-bold">
+                  Masai School <br />
+                  Patna Area, India
+                </p>
+
+                <p className="lead ">
+                  Full-stack web and Computer Science Program Learning MERN
+                  Stack, Data Structures and Algorithms
+                </p>
+                <div className="flex">
+                  <p>
+                    <a href="http://github.com/dipanshuraz/">Github</a>
+                  </p>
+                  <p>
+                    <a href="https://twitter.com/206ff13d95824f3">Twitter</a>
+                  </p>
+                  <p>
+                    <a href="https://www.linkedin.com/in/codersadhu/">
+                      Linkedin
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Achievements;
